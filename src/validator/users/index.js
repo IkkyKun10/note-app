443253721f8fb94ca9b9b373eff285659621e191
riezkymaisyar <riezkymaisyar10@gmail.com>
2023-07-02 01:44:14 +0700

    user registration implemented

---
+++ src/validator/users/index.js
@@ -0,0 +1,15 @@
+const { payload } = require("@hapi/hapi/lib/validation");
+const { UserPayloadSchema } = require("./schema");
+const InvariantError = require("../../exceptions/InvariantError");
+
+const UsersValidator = {
+    validateUserPayload: (payload) => {
+        const validationResult = UserPayloadSchema.validate(payload)
+
+        if (validationResult.error) {
+            throw new InvariantError(validationResult.error.message)
+        }
+    }
+}
+
+module.exports = UsersValidator
